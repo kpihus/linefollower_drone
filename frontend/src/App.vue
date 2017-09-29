3913528785f759a807f0ced1f3710067aaa5b680
Kristjan Pihus <kristjan.pihus@eesti.ee>
2017-09-30 01:28:03 +0300

    Proof of concept

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div class="container-fluid">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- main.js
+++ main.js
@@ -2,17 +2,12 @@ const {fork} = require('child_process');
 const bunyan = require('bunyan');
 const express = require('express');
 const app = express();
-
-app.use(express.static('public'));
-
-const fccomm = fork('./fccomm');
-
+const expressWs = require('express-ws')(app);
 
 const log = bunyan.createLogger({name: 'Quad'});
 
 
-
-
+const fccomm = fork('./fccomm');
 fccomm.on('message', function(msg){
   console.log('CHILD MESSAGE', msg);
   switch (msg.type){
@@ -26,7 +21,37 @@ fccomm.on('message', function(msg){
   }
 });
 
+const vision = fork('./vision');
 
+vision.on('message', (msg) =>{
+  switch (msg.type){
+    case 'info':
+      log.info({info: msg.payload});
+      break;
+
+    default:
+      console.log('UNKNOWN MESSAGE', msg);
+      break;
+  }
+});
+//for groundstation
+
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+});
+
+const socketInstances = expressWs.get('/');
+
+const sendMessage = (data) =>{
+  socketInstances.clients.forEach(client => {
+    client.send(data);
+  })
+};
+
+app.use(express.static('frontend'));
 app.listen(3000, ()=>{
   log.info('App is listeing on port 3000')
 });
+
