3d2da32261439755432744b806c35466edb65ecc
kpihus <kpihus@gmail.com>
2019-11-21 20:55:33 +0200

    Video stream receiver

---
+++ !sandbox/sender_cv.py
@@ -0,0 +1,29 @@
+import cv2
+import base64
+import zmq
+import time
+
+context = zmq.Context()
+footage_socket = context.socket(zmq.PUB)
+footage_socket.connect('tcp://localhost:5555')
+
+camera = cv2.VideoCapture(0)  # init the camera
+n = 0
+while True:
+    n += 1
+
+    try:
+        grabbed, frame = camera.read()  # grab the current frame
+        frame = cv2.resize(frame, (640, 480))  # resize the frame
+        now = time.time()
+        encoded, buffer = cv2.imencode('.jpg', frame)
+        jpg_as_text = base64.b64encode(buffer)
+        footage_socket.send(jpg_as_text)
+
+    except KeyboardInterrupt:
+        camera.release()
+        cv2.destroyAllWindows()
+        break
+    print(str(n), str(time.time() - now))
+    if n > 1000:
+        exit()
--- !sandbox/test.py
+++ !sandbox/test.py
@@ -1,3 +1,30 @@
-import math
+import cv2
+import numpy as np
 
-print(math.degrees(3 * math.pi/2))
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('image', (2600,2600))
+# cv2.setWindowProperty('image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN);
+
+cap = cv2.VideoCapture(0)
+
+# Check if the webcam is opened correctly
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    ret, image = cap.read()
+    frame = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    thresh = cv2.threshold(frame, 130, 200, cv2.THRESH_BINARY)[1]
+
+    vis = np.concatenate((frame, thresh), axis=1)
+    cv2.imshow('image', vis)
+    # cv2.imshow('thresh', thresh)
+    # cv2.moveWindow('image', 0, 0)
+    # cv2.moveWindow('thresh', 700, 0)
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
